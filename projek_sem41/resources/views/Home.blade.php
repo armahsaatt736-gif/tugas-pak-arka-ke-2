485215ccf663e6d87ce107a904bf3397e457f882
arma <emailkamu@gmail.com>
2026-04-20 16:00:38 +0700

    first commit

---
+++ projek_sem41/resources/views/Home.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <a href="/">Home</a>
+    <a href="/kontak">kontak</a>
+    <a href="/profil">profil</a>
+
+
+    <h1>Halaman Home</h1>
+</div>
--- projek_sem41/routes/web.php
+++ projek_sem41/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\controllers\Homecontroller;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Homecontroller::class, 'index']);
