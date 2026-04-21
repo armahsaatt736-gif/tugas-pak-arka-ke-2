0ec9fd9dfc80419c50fc5538e873d2ee743d2893
arma <emailkamu@gmail.com>
2026-04-21 09:39:57 +0700

    second projek

---
+++ projek_sem41/resources/views/Profil.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <a href="/">Home</a>
+    <a href="/kontak">kontak</a>
+    <a href="/profil">profil</a>
+
+    <h1>Halaman Profil</h1>
+    <p>Nama: Arma</p>
+</div>
--- projek_sem41/routes/web.php
+++ projek_sem41/routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\Homecontroller;
+use App\Http\Controllers\ProfilController;
 
 Route::get('/', [Homecontroller::class, 'index']);
+Route::get('/profil', [ProfilController::class, 'index']);
