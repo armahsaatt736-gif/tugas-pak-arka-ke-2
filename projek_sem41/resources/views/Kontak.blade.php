003751cf7fa84232feac34fa542fd93ad7da5a14
arma <emailkamu@gmail.com>
2026-04-21 09:53:11 +0700

    projek ke 3

---
+++ projek_sem41/resources/views/Kontak.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <a href="/">Home</a>
+    <a href="/profil">Profil</a>
+    <a href="/kontak">Kontak</a>
+
+    <h1>Halaman Kontak</h1>
+    <p>Email: arma@email.com</p>
+</div>
--- projek_sem41/routes/web.php
+++ projek_sem41/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\Homecontroller;
 use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\KontakController;
 
 Route::get('/', [Homecontroller::class, 'index']);
 Route::get('/profil', [ProfilController::class, 'index']);
+Route::get('/kontak', [KontakController::class, 'index']);
